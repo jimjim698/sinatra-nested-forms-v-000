7c40456bacf6076bc1b89095ff62b489d2f022d0
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-14 00:06:54 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,11 +1,19 @@
 class Ship
   attr_reader :name, :type, :booty
-  @@ALL = []
+  @@all = []
   def initialize(params)
     @name = params[name]
     @type = params[type]
     @booty = params[booty]
-    ALL << self
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
   end 
 
 end
